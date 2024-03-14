c8e668e5d34aa96dc816d7839805bf3935fdb38d
dimas yudha <137317128+dimasyudhasaputra@users.noreply.github.com>
2024-03-14 16:40:58 +0700

    Add files via upload

---
+++ Pertemuan_4/tugas_tambahan/academic/Course.java
@@ -0,0 +1,67 @@
+package tugas_tambahan.academic;
+
+/*
+NAMA : DIMAS YUDHA SAPUTRA
+NIM : 24060122120025
+LAB : PBO D1
+PERTEMUAN : 2
+*/
+import java.util.List;
+import java.util.ArrayList;
+
+public class Course {
+    private String courseCode;
+    private String courseName;
+    private Lecture lecture;
+    private List<Student> studentsEnrolled;
+
+    public Course(String courseCode, String courseName, Lecture lecture) {
+        this.studentsEnrolled = new ArrayList<>();
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.lecture = lecture;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public Lecture getLecture() {
+        return lecture;
+    }
+
+    public void addStudent(Student student) {
+        studentsEnrolled.add(student);
+        System.out.println(student.getName() + " didaftarkan ke kelas " + courseName);
+    }
+
+    public void removeStudent(Student student) {
+        studentsEnrolled.remove(student);
+        System.out.println(student.getName() + " dihapus dari kelas " + courseName);
+    }
+
+    public void viewEnrolledStudents() {
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Students yang terdaftar di course " + courseName + " :");
+        System.out.println("----------------------------------------------------------------");
+
+        if (studentsEnrolled.isEmpty()) {
+            System.out.println("Tidak ada students yang terdaftar di kelas ini.");
+        } else {
+            for (Student student : studentsEnrolled) {
+                student.getDetails();
+                System.out.println("----------------------------------------------------------------");
+            }
+        }
+    }
+
+    public void getDetails() {
+        System.out.println("  Course Code: " + courseCode);
+        System.out.println("  Course Name: " + courseName);
+        System.out.println("  Lecture: " + lecture.getName());
+    }
+}
