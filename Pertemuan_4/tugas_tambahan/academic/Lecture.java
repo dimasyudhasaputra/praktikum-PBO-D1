17b657b3b7898b4942afce7ce9c3bfda5113298b
dimas yudha <137317128+dimasyudhasaputra@users.noreply.github.com>
2024-03-14 22:50:55 +0700

    Add files via upload

---
+++ Pertemuan_4/tugas_tambahan/academic/Lecture.java
@@ -0,0 +1,55 @@
+/*
+NAMA : DIMAS YUDHA SAPUTRA
+NIM : 24060122120025
+LAB : PBO D1
+PERTEMUAN : 4
+*/
+
+package tugas_tambahan.academic;
+
+import tugas_tambahan.person.Person;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Lecture extends Person {
+
+    private int employeeID;
+    private List<Course> coursesTaught;
+
+    public Lecture(String name, int age, String address, int employeeID) {
+        super(name, age, address);
+        this.employeeID = employeeID;
+        this.coursesTaught = new ArrayList<>();
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void getDetails() {
+        System.out.println(" Name: " + name);
+        System.out.println(" Age: " + age);
+        System.out.println(" Address: " + address);
+        System.out.println(" Employee ID: " + employeeID);
+    }
+
+    public void teachCourse(Course course) {
+        coursesTaught.add(course);
+        System.out.println(name + " mengampu kelas : " + course.getCourseName());
+    }
+
+    public void viewTaughtCourses() {
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Kelas yang diampu oleh dosen " + name);
+        System.out.println("----------------------------------------------------------------");
+
+        if (coursesTaught.isEmpty()) {
+            System.out.println("Dosen ini tidak mengampu kelas sama sekali.");
+        } else {
+            for (Course course : coursesTaught) {
+                course.getDetails();
+                System.out.println("----------------------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ Pertemuan_4/tugas_tambahan/person/Person.java
@@ -0,0 +1,38 @@
+/*
+NAMA : DIMAS YUDHA SAPUTRA
+NIM : 24060122120025
+LAB : PBO D1
+PERTEMUAN : 4
+*/
+
+package tugas_tambahan.person;
+
+public class Person {
+    protected String name;
+    protected int age;
+    protected String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void getDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+    }
+}
